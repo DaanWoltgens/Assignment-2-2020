4059d542e390ff4cdfccc039e22fd678455b43b5
DaanWöltgens <d.p.woltgens@student.tue.nl>
2021-03-08 19:33:43 +0100

    Added more duplication examples and edited Dockerfile
    Edited dockerfile to copy the manual examples to the vm

--- manual-clones/type3.js
+++ manual-clones/type3.js
@@ -1,32 +1,7 @@
-function function1(p1, p2) {
-    return p1 * p2;
-}
-
-function function1copy(p1, p2) {
-    return p1*p2;
-}
-
-function function2(p1, p2) {
-    while(p1 > 0){
-        p2 += p1;
-        p1--;
-    }
-    return p2;
-}
-
-function function2copy(p1,p2) {
-   while(p1 > 0){
-p2 += p1;
- p1--;
-    }
-        return p2;
-}
-
 function function3(p1,p2,p3,p4) {
     p5 = p1 + p2;
     p6 = p3 + p4;
     while(p5 > p6){
-        p1 += p2;
         p3 = p1 + p4;
         p5 -= p1;
     }
@@ -48,14 +23,18 @@ return p3;
 
 function function4(a,b){
     if(a<0 || b<0){
+        print("An extra statement");
         return 0;
     }
+    d = a *b;
+    e = d*b;
     return a^2 + b^2
 }
 
 function function4copy(a,b){
     //This checks for negative values
-if(a<0 || b<0 ){
+if(a<0 || b<0){
+    d = a+b
   return 0;
 }
     //This returns the length of the hypotenuse of a triangle
