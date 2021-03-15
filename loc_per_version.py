48336d82919e7c3e11fe35f8e68d3b263ed4ea3e
Youri Sio <yourisio@gmail.com>
2021-03-15 14:22:37 +0100

    Fix error in loc

--- loc_per_version.py
+++ loc_per_version.py
@@ -20,5 +20,5 @@ for version in versions:
     result = subprocess.run(['cloc', '--exclude-ext=json', '--csv', '--quiet', f'./{version}/src'], stdout=subprocess.PIPE) # cloc --exclude-ext=json ./src --csv --quiet
     result_str = result.stdout.decode('utf8')
     js_info = result_str.split('\n')[2].split(',')
-    loc = int(js_info[4]) - int(js_info[3]) - int(js_info[2])
+    loc = int(js_info[4])
     print(f'{version},{loc}')
